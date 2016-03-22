321bc513ee8ca76e9f2b9ce33595883a2e144a8a
okiloco <okiloco2@gmail.com>
2016-03-22 08:50:12 -0500

    Notrash Project

---
+++ js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('registrarCtrl', function($scope) {
+
+})
+      
+.controller('recursosCtrl', function($scope) {
+
+})
+   
+.controller('recursoDetalleCtrl', function($scope) {
+
+})
+   
+.controller('recursoCtrl', function($scope) {
+
+})
+   
+.controller('etiquetasCtrl', function($scope) {
+
+})
+   
+.controller('comentariosCtrl', function($scope) {
+
+})
+ 
---
+++ js/routes.js
@@ -0,0 +1,77 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.login', {
+    url: '/login',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+
+  .state('registrar', {
+    url: '/registrar',
+    templateUrl: 'templates/registrar.html',
+    controller: 'registrarCtrl'
+  })
+
+  .state('menu', {
+    url: '/menu',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+  .state('recursos', {
+    url: '/recursos',
+    templateUrl: 'templates/recursos.html',
+    controller: 'recursosCtrl'
+  })
+
+  .state('recursoDetalle', {
+    url: '/recursoDetalle',
+    templateUrl: 'templates/recursoDetalle.html',
+    controller: 'recursoDetalleCtrl'
+  })
+
+  .state('menu.recurso', {
+    url: '/recurso/:id',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/recurso.html',
+        controller: 'recursoCtrl'
+      }
+    }
+  })
+
+  .state('menu.etiquetas', {
+    url: '/etiquetas',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/etiquetas.html',
+        controller: 'etiquetasCtrl'
+      }
+    }
+  })
+
+  .state('comentarios', {
+    url: '/comentarios',
+    templateUrl: 'templates/comentarios.html',
+    controller: 'comentariosCtrl'
+  })
+
+$urlRouterProvider.otherwise('/menu/login')
+
+  
+
+});
